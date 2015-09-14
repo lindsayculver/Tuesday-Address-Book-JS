5d925b9d8cc0062ef60daba4935cfd4a43283551
Yelena <ynedelko@gmail.com>
2015-09-14 11:06:04 -0700

    Add ability to view adress detials upon click.

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,13 @@ $(document).ready(function() {
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-address").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".address").text(newContact.address);
+    });
   });
 });
