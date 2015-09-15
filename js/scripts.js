7d859554d5bb933aacf89531a0bea2e73b077bda
Yelena Nedelko & Lindsay Culver <student@epicodus.com>
2015-09-15 10:06:48 -0700

    Add resetFields function.

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,13 @@ Address.prototype.fullAddress = function() {
   return this.street + ", " + this.city + ", " + this.state;
 }
 
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+}
 
 $(document).ready(function() {
   $("#add-address").click(function() {
@@ -69,10 +76,12 @@ $(document).ready(function() {
       });
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+    resetFields();
+
+    // $("input#new-first-name").val("");
+    // $("input#new-last-name").val("");
+    // $("input.new-street").val("");
+    // $("input.new-city").val("");
+    // $("input.new-state").val("");
   });
 });
